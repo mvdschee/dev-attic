d7d7589e40c8ff9ef7d1feff028dc29b4d3dc7a8
Max van der Schee <m.v.d.schee@ewake.nl>
2019-01-26 22:09:21 +0100

    added router animations and update angular to 7.2

--- src/app/router.animations.ts
+++ src/app/router.animations.ts
@@ -1,27 +1,24 @@
-import { trigger, animate, style, group, animateChild, query, stagger, transition } from '@angular/animations';
+import { trigger, animate, transition, style, query} from '@angular/animations';
 
 export const routerTransition = trigger('routerTransition', [
-    // transition('* <=> *', [
-    // /* order */
-    // /* 1 */ query(':enter, :leave', style({ position: 'fixed', width: '100%' }),
-    //             { optional: true }),
-    // /* 2 */ query('.post-item', style({ opacity: 0 }),
-    //             { optional: true }),
-    // /* 2 */ group([
-    //             query(':enter', [
-    //                 style({ transform: 'translateY(100%)' }),
-    //                 animate('0.5s ease-in-out', style({ transform: 'translateY(0%)' }))
-    //             ], { optional: true }),
-    //             query(':leave', [
-    //                 style({ transform: 'translateY(0%)' }),
-    //                 animate('0.5s ease-in-out', style({ transform: 'translateY(-100%)' })),
-    //             ], { optional: true }),
-    //             query(':enter .post-item', [stagger(200, [
-    //                 style({ transform: 'translateY(100px)' }),
-    //                 animate('1s ease-in-out',
-    //                 style({ transform: 'translateY(0px)', opacity: 1 })),
-    //             ])],
-    //             { optional: true })
-    //     ])
-    // ])
+    transition('* <=> *', [
+        query(':enter', [
+            style({
+                transform: 'translateY(-1rem)',
+                opacity: '0'
+            }),
+            animate('300ms 300ms ease-in-out', style({
+                transform: 'translateY(0)',
+                opacity: '1'
+            })),
+        ], { optional: true }),
+        query(':leave', [
+            style({
+                opacity: '1'
+            }),
+            animate('300ms ease-in-out', style({
+                opacity: '0'
+            })),
+        ], { optional: true }),
+    ]),
 ]);
