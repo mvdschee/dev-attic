ca4c7d23c7b1fc116bf375b86b1dc8cd29092362
Max van der Schee <m.v.d.schee@ewake.nl>
2019-01-26 21:50:39 +0100

    Removed route in env for external resources

--- src/app/posts.ts
+++ src/app/posts.ts
@@ -1,30 +1,37 @@
 export const posts = [
     {
         'title': 'Audio Usability',
-        'file': 'audio-usability/usability.html',
+        'file': 'https://dev-attic.com/development/audio-usability/usability.html',
         'content': 'Audio as indication where to start',
-        'image': 'audio-usability/preview.png',
+        'image': 'https://dev-attic.com/development/audio-usability/preview.png',
         'medium': 'https://medium.com/@maxvanderschee/the-muted-web-and-how-to-fix-it-5bdb65362e36'
     },
     {
         'title': 'Audio Atmosphere',
-        'file': 'audio-atmosphere/atmosphere.html',
+        'file': 'https://dev-attic.com/development/audio-atmosphere/atmosphere.html',
         'content': 'Atmospheric audio to add more depth to the content',
-        'image': 'audio-usability/preview.png',
+        'image': 'https://dev-attic.com/development/audio-usability/preview.png',
         'medium': 'https://medium.com/@maxvanderschee/the-muted-web-and-how-to-fix-it-5bdb65362e36'
     },
     {
         'title': 'Audio Satisfaction',
-        'file': 'audio-satisfaction/satisfaction.html',
+        'file': 'https://dev-attic.com/development/audio-satisfaction/satisfaction.html',
         'content': 'Audio giving an interaction more satisfaction',
-        'image': 'audio-usability/preview.png',
+        'image': 'https://dev-attic.com/development/audio-usability/preview.png',
         'medium': 'https://medium.com/@maxvanderschee/the-muted-web-and-how-to-fix-it-5bdb65362e36'
     },
     {
         'title': 'Peer-to-peer',
-        'file': 'peer-to-peer-web-content/index.html',
+        'file': 'https://dev-attic.com/development/peer-to-peer-web-content/index.html',
         'content': 'Peer-to-Peer content delivery',
-        'image': 'peer-to-peer-web-content/preview.png',
-        'medium': ''
+        'image': 'https://dev-attic.com/development/peer-to-peer-web-content/preview.png',
+        'medium': 'https://medium.com/@maxvanderschee/the-power-of-peer-to-peer-right-in-your-browser-16fbeff0d44d'
+    },
+    {
+        'title': 'Web Accessibility',
+        'file': 'https://marketplace.visualstudio.com/items?itemName=MaxvanderSchee.web-accessibility',
+        'content': 'Visual Studio Code Extension',
+        'image': 'https://github.com/mvdschee/web-accessibility/raw/master/./web-accessibility.gif',
+        'medium': 'https://medium.com/@maxvanderschee/why-the-adoption-of-web-accessibility-keeps-failing-e78fe4c94149'
     },
 ];
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
-  route: 'https://demo.dev-attic.com/'
+  production: true
 };
